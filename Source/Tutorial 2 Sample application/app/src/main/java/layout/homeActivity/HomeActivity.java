f119565bab4ba30caa06c3f420a7c558d9037b4a
Ragunandan Rao <ragunandanrao@gmail.com>
2016-02-03 18:10:42 -0600

    Added the task for lab 2

---
+++ Source/Tutorial 2 Sample application/app/src/main/java/layout/homeActivity/HomeActivity.java
@@ -0,0 +1,156 @@
+package layout.homeActivity;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import domain.raghu.myapplication.R;
+import domain.raghu.myapplication.Sample;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HomeActivity extends AppCompatActivity {
+    String API_URL = "https://api.fullcontact.com/v2/person.json?";
+    String API_KEY = "b29103a702edd6a";
+    String sourceText;
+    TextView outputTextView;
+    Context mContext;
+    WebView weatherInfo;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getBaseContext();
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        outputTextView = (TextView) findViewById(R.id.txt_Result);
+    }
+
+    public void logout(View v) {
+        Intent redirect = new Intent(HomeActivity.this, Sample.class);
+        startActivity(redirect);
+    }
+
+    private void hideKeyboard(View editableView) {
+        InputMethodManager imm = (InputMethodManager)mContext
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editableView.getWindowToken(), 0);
+    }
+
+    public void translateText(View v) {
+        TextView sourceTextView = (TextView) findViewById(R.id.txt_Email);
+
+        sourceText = sourceTextView.getText().toString();
+        String getURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?" +
+                "key=trnsl.1.1.20151023T145251Z.bf1ca7097253ff7e." +
+                "c0b0a88bea31ba51f72504cc0cc42cf891ed90d2&text=" + sourceText +"&" +
+                "lang=en-es&[format=plain]&[options=1]&[callback=set]";//The API service URL
+        final String response1 = "";
+        OkHttpClient client = new OkHttpClient();
+        try {
+
+            Request request = new Request.Builder()
+                    .url(getURL)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                   final JSONObject jsonResult;
+                    final String result = response.body().string();
+
+                    try {
+                        jsonResult = new JSONObject(result);
+                        JSONArray convertedTextArray = jsonResult.getJSONArray("text");
+                        final String convertedText = convertedTextArray.get(0).toString();
+                        Log.d("okHttp", jsonResult.toString());
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                hideKeyboard(outputTextView);
+                                int notificationId = 100;
+                                NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(HomeActivity.this)
+                                        .setSmallIcon(R.drawable.notificationicon)
+                                        .setContentTitle("Converted text")
+                                        .setContentText(convertedText);
+                                NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(HomeActivity.this);
+
+                                notificationManagerCompat.notify(notificationId, notificationBuilder.build());
+                                outputTextView.setText(convertedText);
+                            }
+                        });
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+
+
+        } catch (Exception ex) {
+            outputTextView.setText(ex.getMessage());
+
+        }
+
+    }
+
+}
+
+
